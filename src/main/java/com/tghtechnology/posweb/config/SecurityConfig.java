205598d30ac3c28f02f375d040d18cc273b4e181
Xtian2207 <montenegroc322@gmail.com>
2025-04-05 15:24:30 -0500

    render

--- src/main/java/com/tghtechnology/posweb/config/SecurityConfig.java
+++ src/main/java/com/tghtechnology/posweb/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://punto-de-venta.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
